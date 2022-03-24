4699124fda37677593247f9f5841ea34e61521c8
Marshall Baughman <95940057+mbaughman7@users.noreply.github.com>
2022-03-24 17:05:37 -0500

    initial commit of NASA app starter file

---
+++ NASA_app.py
@@ -0,0 +1,16 @@
+import requests
+from datetime import datetime
+import tkinter
+
+
+# find NASA APIs that are available for GET requests.
+# start with one.  A dropdown menu will come later. First,
+# do one mission.
+# Create button that executes the GET request (no parameters, at first)
+#
+# output will depend on the payload of the response from the API.
+# consider payload of csv in txt file or an image file.
+# possibly, output is simply info displayed in a text box on tkinter box.
+# good starting place is US map game, pomadoro timer project, and iss_overhead project
+
+
