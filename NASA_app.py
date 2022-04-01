fe866dcd10604dcacef87b37d58af18e3ef4a212
Marshall Baughman <95940057+mbaughman7@users.noreply.github.com>
2022-03-31 21:38:09 -0500

    Update NASA_app.py
    program sends GET request to NASA DONKI API for CME data.  Program prints a list of CMEs within last 30 days that either have or are predicted to impact Earth.  
    PROBLEM: as it is currently written, the program will simply create an empty text file the first time it is run.  Once the text file is created, the program works fine.

--- NASA_app.py
+++ NASA_app.py
@@ -1,16 +1,53 @@
 import requests
 from datetime import datetime
 import tkinter
+# from nasa_data import data
 
 
-# find NASA APIs that are available for GET requests.
-# start with one.  A dropdown menu will come later. First,
-# do one mission.
+response = requests.get("https://api.nasa.gov/DONKI/CME?api_key=DEMO_KEY")
+data = response.json()
+print(data)
+cme_list = []
+
+for item in data:
+    try:
+        earth_hit = item["cmeAnalyses"][0]["enlilList"][0]["estimatedShockArrivalTime"]
+    except TypeError:
+        pass
+        # print("error here.  Probably a null")
+    else:
+        if earth_hit:
+            name = item["activityID"]
+            impact_time = item["cmeAnalyses"][0]["enlilList"][0]["estimatedShockArrivalTime"]
+            clean_time_date = impact_time.split("T")[0]
+            clean_time = impact_time.split("T")[1].split("Z")[0]
+            entry = f"CME identified as {name} will impact earth on {clean_time_date} at approx {clean_time}"
+            cme_list.append(entry)
+### FOR TROUBLESHOOTING
+# for i in enumerate(cme_list):
+#     print(i)
+try:
+    with open("CME_events.txt",'r') as my_file:
+        cme_events = my_file.readlines()
+        ### FOR TROUBLESHOOTING
+        # for line in cme_events:
+        #     print(f"this is from file: {line}")
+    with open("CME_events.txt",'w') as my_file:
+        for item in cme_list:
+            if item in cme_events:
+                continue
+            my_file.write('%s\n' % item)
+            print(item)
+            
+except FileNotFoundError:
+    with open("CME_events.txt",'w') as my_file:
+        my_file.write("")
+        
+#TODO this part needs to be cleaned up.  As written, the first time the program is run,
+# it will simply create a file and then exit.  
+
+
+
 # Create button that executes the GET request (no parameters, at first)
-#
-# output will depend on the payload of the response from the API.
-# consider payload of csv in txt file or an image file.
-# possibly, output is simply info displayed in a text box on tkinter box.
-# good starting place is US map game, pomadoro timer project, and iss_overhead project
 
 
